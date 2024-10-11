62999031dd7e2e509106a7fe1d7ac7ff3c66107b
Jennifer Chang <jennifer.chang.bioinform@gmail.com>
2024-10-11 11:43:24 -0700

    basic phylogenetic analysis

---
+++ run-phylogenetic-analysis.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+set -euv
+
+INPUT_SEQUENCE_FILE=$1
+OUTDIR=results
+
+[[ -d $OUTDIR ]] || mkdir $OUTDIR
+
+mafft --auto $INPUT_SEQUENCE_FILE > ${OUTDIR}/aligned.fasta
+
+fasttree -nt ${OUTDIR}/aligned.fasta > ${OUTDIR}/tree.nwk
+
+# Open in FigTree
+# Midpoint Root
+# Start pulling out outliers
+# Start adding filtering
+# Start adding metadata and subsampling
+# Remove duplicates
